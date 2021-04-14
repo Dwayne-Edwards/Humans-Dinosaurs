268afd644db17855251e15df2bceecf36284d498
Dwayne Edwards <dwayne.eds@gmail.com>
2021-04-14 12:02:39 +0800

    refactor: Move form validation to its own module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import dinos from './dino.js';
-import dom$ from './modules/domcahce.js'
+import dom$ from './modules/domcahce.js';
+import formValidation from './modules/form.js';
     
 // Create Dino Constructor
 function Dinosaur (dino){
@@ -303,81 +304,81 @@ function generateTileGrid(dino, userObj, unit){
     })
 } 
 
-// Add form Validation
-const formValidation = (function () {
-    let formFields = {
-        name: false,
-        feet: false,
-        inches: false,
-        weight: false
-    }
-    function markInvalid(id){
-        let element = dom$.get(id);
-        element.className += " form-field_invalid";
-        element.className = element.className.replace(/\bform-field_valid\b/g, "");
-    }
+// // Add form Validation
+// const formValidation = (function () {
+//     let formFields = {
+//         name: false,
+//         feet: false,
+//         inches: false,
+//         weight: false
+//     }
+//     function markInvalid(id){
+//         let element = dom$.get(id);
+//         element.className += " form-field_invalid";
+//         element.className = element.className.replace(/\bform-field_valid\b/g, "");
+//     }
 
-    function markValid(id){
-        let element = dom$.get(id);
-        element.className += " form-field_valid";
-        element.className = element.className.replace(/\bform-field_invalid\b/g, "");
-    }
+//     function markValid(id){
+//         let element = dom$.get(id);
+//         element.className += " form-field_valid";
+//         element.className = element.className.replace(/\bform-field_invalid\b/g, "");
+//     }
 
-    function checkValidity(id, value){
-        switch(id){
-            case 'name':
-                if(value.length < 1 || value === ' '){
-                    markInvalid(id);
-                    return false;
-                }
-                markValid(id);
-                return true;
-            case 'feet':
-            case 'inches':
-                let height = parseInt(dom$.get('feet').value, 10) + parseInt(dom$.get('inches').value, 10)
-                if( height > 0){
-                    formFields['feet'] = true;
-                    formFields['inches'] = true;
-                    markValid('feet');
-                    markValid('inches');
-                    return true;
-                } else {
-                    formFields['feet'] = false;
-                    formFields['inches'] = false;
-                    markInvalid('feet');
-                    markInvalid('inches'); 
-                    return false;
-                };
+//     function checkValidity(id, value){
+//         switch(id){
+//             case 'name':
+//                 if(value.length < 1 || value === ' '){
+//                     markInvalid(id);
+//                     return false;
+//                 }
+//                 markValid(id);
+//                 return true;
+//             case 'feet':
+//             case 'inches':
+//                 let height = parseInt(dom$.get('feet').value, 10) + parseInt(dom$.get('inches').value, 10)
+//                 if( height > 0){
+//                     formFields['feet'] = true;
+//                     formFields['inches'] = true;
+//                     markValid('feet');
+//                     markValid('inches');
+//                     return true;
+//                 } else {
+//                     formFields['feet'] = false;
+//                     formFields['inches'] = false;
+//                     markInvalid('feet');
+//                     markInvalid('inches'); 
+//                     return false;
+//                 };
                 
-            case 'weight':
-                if(isNaN(value) || value < 1){
-                    markInvalid(id);
-                    return false;
-                }
-                markValid(id);
-                return true;
-        }
-    }
+//             case 'weight':
+//                 if(isNaN(value) || value < 1){
+//                     markInvalid(id);
+//                     return false;
+//                 }
+//                 markValid(id);
+//                 return true;
+//         }
+//     }
 
-    function setState(id, value){
-        formFields[id] = checkValidity(id, value);
-    }
+//     function setState(id, value){
+//         formFields[id] = checkValidity(id, value);
+//     }
 
-    function validateFormState(){
-        let state = true;
-        for(const element in formFields){
-            if(formFields[element] === false){
-                state = false;
-            }
-        };
-        return state;
-    }
+//     function validateFormState(){
+//         let state = true;
+//         for(const element in formFields){
+//             if(formFields[element] === false){
+//                 state = false;
+//             }
+//         };
+//         return state;
+//     }
 
-    return {
-        get: validateFormState,
-        set: setState
-    }
-})();
+//     return {
+//         get: validateFormState,
+//         set: setState
+//     }
+// })();
 
 
 // Toggle form units between Imperial and Metric
