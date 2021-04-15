e39664a0f41bb9b35ed9bffba378cac0ca5b3fe2
Dwayne Edwards <dwayne.eds@gmail.com>
2021-04-15 10:58:11 +0800

    docs: add funtionality comments

--- modules/units.js
+++ modules/units.js
@@ -18,7 +18,7 @@ export function imperialMetricSwap(id, height, weight, unit){
 }
 
 /**
-* @description Loop through a list DOM elements by id and call a swapping function to toggle between imperial and metric 
+* @description Loop through a list DOM elements by id and call a swapping function to toggle between imperial and metric
 * @param {number} height - inches or centimeters
 * @param {number} weight - pounds or kgs
 * @param {string} unit - accepted values:  'imperial' or 'metric'
@@ -32,7 +32,10 @@ export function toggleValues(height, weight, unit, elementIds) {
 };
 
 
-//   Toggle the form's displayed units and update the [unit] checkbox value
+/**
+* @description Toggle a form's displayed label and update the [unit] checkbox value
+* @param {array} elementIds - Array of DOM elements ids
+*/
 export function toggleUnits(unit) {
     let isImperial = unit === 'imperial';
     dom$.get('unitLabel').innerText = isImperial ? 'Uncheck for imperial': 'Switch to metric';
@@ -42,14 +45,32 @@ export function toggleUnits(unit) {
 };
 
 
+/**
+* @description Reduce a pair of imperial value inputs to a single output
+* @param {number} ft - positive integer
+* @param {number} inches - positive integer
+* @returns {number} The sum of ft and inches converted to inches
+*/
 export function reduceToInches(ft, inches){
     return parseInt(ft, 10) * 12 + parseInt(inches, 10);
 }
 
+/**
+* @description Reduce a pair of imperial value inputs to a single output
+* @param {number} m - positive integer in meters
+* @param {number} cm - positive integer in centimeters
+* @returns {number} The sum of m and cm converted to cm
+*/
 export function reduceToCentimeters(m, cm){
     return parseInt(m, 10) * 100 + parseInt(cm, 10);
 }
 
+/**
+* @description Toggles the input value between imperial and metric
+* @param {number} height - positive integer in meters
+* @param {string} unit - accepted values:  'imperial' or 'metric'
+* @returns {number} an integer that is inverse of the input unit
+*/
 export function toggleHeight(height, unit){
     if(unit === 'imperial'){
         return Math.round(parseInt(height, 10) * 2.54);
@@ -57,6 +78,14 @@ export function toggleHeight(height, unit){
     return Math.round(parseInt(height, 10) / 2.54);
 }
 
+/**
+* @description Toggles the input value between imperial and metric
+* @param {number} h1 - positive integer representing feet or meters
+* @param {number} h2 - positive integer representing inches or centimeters
+* @param {number} weight - positive integer in pounds or kilograms
+* @param {string} unit - accepted values:  'imperial' or 'metric'
+* @param {array} elementIds - Array of DOM element[ids] values to toggle
+*/
 export function toggleUnitsAndValues(h1, h2, weight, unit, elementIds){
     let height = 0;
     if(unit === 'imperial'){
@@ -68,12 +97,15 @@ export function toggleUnitsAndValues(h1, h2, weight, unit, elementIds){
     toggleUnits(unit);
 }
 
-// Normalize the comparing values
-// Imperial is reduced to inches
-// Metric is reduced to centimeters
+/**
+* @description Normalize a value, where imperial reduce to inches and metric to centimeters
+* @param {number || array} num - positive integer or array of two integer representing feet/inches or meters/centimeters
+* @param {string} unit - accepted values:  'imperial' or 'metric'
+* @returns {number} returns the input value reduced to inches or centimeters
+*/
 export function normalizeHeight(num, unit){
     let height;
-        // If user is using imperial units
+        // User is using imperial units
         if(unit === 'imperial' && Array.isArray(num)){
             return height =  reduceToInches(num[0], num[1]);
         }
@@ -82,7 +114,7 @@ export function normalizeHeight(num, unit){
             return height =  reduceToCentimeters(num[0], num[1]);
         }
 
-        // If values are in imperial and the user is using metric
+        // Values are in imperial and the user is using metric
         if(unit !== 'imperial'){
            return height = toggleHeight(parseInt(num), 'imperial');
         }
@@ -92,9 +124,15 @@ export function normalizeHeight(num, unit){
 
 };
 
+/**
+* @description Normalize a value, where pounds converted to kilograms
+* @param {number} num - positive number representing pounds or kiograms
+* @param {string} unit - accepted values:  'imperial' or 'metric'
+* @returns {number} returns the input value converted to kilograms
+*/
 export function normalizeWeight(num, unit){
     let weight;
-        // If user is using metric units
+        // User is using metric units
         if(unit === 'metric'){
             return weight =  (parseFloat(num) / 2.203).toFixed(1);
         }
